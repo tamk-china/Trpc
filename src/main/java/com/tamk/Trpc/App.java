ae2aac700e234735257c6bed125e7a3a379eecb7
kuanqiang.tkq <kuanqiang.tkq@alibaba-inc.com>
2016-05-08 19:58:32 +0800

    ticket

---
+++ src/main/java/com/tamk/Trpc/App.java
@@ -0,0 +1,13 @@
+package com.tamk.Trpc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
